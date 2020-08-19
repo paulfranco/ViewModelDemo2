97b4c47ddb499e01bdbe0461b0b2147f2409fb9c
Paul Franco <mail@paulfran.co>
2020-08-19 05:07:42 -0700

    Added LiveData

--- app/src/main/java/co/paulfran/viewmodeldemo2/MainActivity.kt
+++ app/src/main/java/co/paulfran/viewmodeldemo2/MainActivity.kt
@@ -3,6 +3,7 @@ package co.paulfran.viewmodeldemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import co.paulfran.viewmodeldemo2.databinding.ActivityMainBinding
 
@@ -17,11 +18,13 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
-        binding.resultTV.text = viewModel.getTotal().toString()
+
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTV.text = it.toString()
+        })
 
         binding.submitBtn.setOnClickListener {
             viewModel.setTotal(binding.inputET.text.toString().toInt())
-            binding.resultTV.text = viewModel.getTotal().toString()
         }
     }
 }
--- app/src/main/java/co/paulfran/viewmodeldemo2/MainActivityViewModel.kt
+++ app/src/main/java/co/paulfran/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,19 +1,23 @@
 package co.paulfran.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int): ViewModel() {
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData: LiveData<Int>
+    get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
-
-    fun getTotal(): Int {
-        return total
-    }
+//     No longer needed since we are going to observe via live data
+//    fun getTotal(): Int {
+//        return total
+//    }
 
     fun setTotal(input: Int) {
-        total += input
+        total.value = (total.value)?.plus(input)
     }
 }
